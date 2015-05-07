52d876d747ea32663c4ca7b641e063d02740f355
deepakds1 <deepak.shanbhag@emids.com>
2015-05-07 17:38:54 +0530

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :projects
-   root :to => 'projects#index'
+   root :to => 'projects#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
